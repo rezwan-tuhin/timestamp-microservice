6fac87ec3b88278925837923f60ee5f94a3ff992
rezwan-tuhin <rezwan.tuhin@gmail.com>
2025-07-12 21:09:53 +0600

    fixed

--- index.js
+++ index.js
@@ -24,15 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api', (req, res) => {
-  const now = new Date();
-    return res.json({
-      unix: now.getTime(),
-      utc : now.toUTCString()
-    });
-})
 
-app.get('/api/:date', (req, res) => {
+
+app.get('/api/:date?', (req, res) => {
 
   const date = req.params.date;
 
